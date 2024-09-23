73c15f6691c759a5e903e0131ddc1133ea9ce316
it24102346 <it24102346@my.sliit.lk>
2024-09-23 11:27:46 +0530

    lab  7 Q2A

---
+++ Q2/IT24102346Lab7Q2A.java
@@ -0,0 +1,16 @@
+public class IT24102346Lab7Q2A {
+    public static void main(String[] args) {
+       
+        int rows = 4;
+        int columns = 5;
+
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("$ ");
+            }
+           
+            System.out.println();
+        }
+    }
+}
